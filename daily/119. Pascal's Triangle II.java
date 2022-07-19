3187ceb5af0c9625535f4b31d90d69898ba432e7
Nikhitha-1612 <96165205+Nikhitha-1612@users.noreply.github.com>
2022-07-19 20:02:41 +0530

    Create 119. Pascal's Triangle II.java

---
+++ daily/119. Pascal's Triangle II.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> pre,row = null;
+        pre = new ArrayList<Integer>();
+        for(int i=0;i<=rowIndex;++i){
+            row = new ArrayList<Integer>();
+            for(int j=0;j<=i;++j)
+                if(j==0||j==i)
+                    row.add(1);
+                else
+                    row.add(pre.get(j-1)+pre.get(j));
+            
+            pre=row;
+        }
+       return row;
+    }
+}
